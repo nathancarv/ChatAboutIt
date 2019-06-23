dbfe7990d50bbbf993dd76915b2e5ac747fa8a63
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-06-22 23:46:12 -0400

    messagelist

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
- class MessageList extends Component {
+class MessageList extends Component {
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+      return (
+        <ul className="message-list">                 
+          {this.props.messages.map(message => {
+            return (
+             <li key={message.id}>
+               <div>
+                 {message.senderId}
+               </div>
+               <div>
+                 {message.text}
+               </div>
+             </li>
+           )
+         })}
+       </ul>
+      )
     }
-}
+  }
 
 export default MessageList;
